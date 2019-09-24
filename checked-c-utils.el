ede574782b2d16c91f2913be57e8988267db5c1d
Aaron Eline <aeline@umd.edu>
2019-09-24 12:53:20 -0400

    Support for replacing includes

--- checked-c-utils.el
+++ checked-c-utils.el
@@ -6,12 +6,23 @@
 ;; Set this variable to your checked-c-convert
 (defvar convert-path "/media/aeline/Extra/checkedc/llvm.obj/bin/checked-c-convert")
 
+(defvar checked-libs
+  '(("<stdio.h>" "<stdio_checked.h>")
+    ("<stdlib.h>" "<stdlib_checked.h>")))
+
 (defun convert ()
   "Convert a given region to checked-c."
   (interactive)
+  (save-excursion
+    (mapcar (lambda (pair)
+              (goto-char 0)
+              (replace-string (car pair) (cadr pair)))
+            checked-libs))
+  (save-buffer)
   (setf (buffer-string) (shell-command-to-string
                          (concat convert-path " "(buffer-file-name)
                                  " 2> /dev/null | sed -e \"s/\r//g\""))))
+
 
 
 (provide 'checked-c-utils)
