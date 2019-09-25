782fed9c0264e35cce2ea1012654eff8feaffa95
Aaron Eline <aeline@umd.edu>
2019-09-24 21:24:51 -0400

    local includes work

--- flychecked-c.el
+++ flychecked-c.el
@@ -12,12 +12,14 @@
   :command (
             "/media/aeline/Extra/checkedc/llvm.obj/bin/clang-8"
             "-fsyntax-only"
+            "-iquote" (eval (flycheck-c/c++-quoted-include-directory))
             "-I /media/aeline/Extra/checkedc/llvm.obj/lib/clang/8.0.0/include/"
             source)
   :error-patterns
   ((error line-start (file-name) ":" line ":" column ": error: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column ": fatal error: " (message) line-end)
    (warning line-start (file-name) ":" line ":" column ": warning: " (message) line-end)
-   (warning line-start (file-name) ": " line ":" column ": note: " (message) line-end)
+   (note line-start (file-name) ":" line ":" column ": note: " (message) line-end)
    )
   :modes c-mode)
 
